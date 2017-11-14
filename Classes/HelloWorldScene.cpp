d57a53ca9f4ea1919c7cc1c982b055162d95daaa
Needseatar <2684587613@.com>
2017-11-14 09:43:08 +0800

    add particle

--- Classes/HelloWorldScene.cpp
+++ Classes/HelloWorldScene.cpp
@@ -228,6 +228,78 @@ bool HelloWorld::init()
         return false;
     };
     
+    
+    
+    /*粒子系统*/
+    auto _emitter = ParticleSystemQuad::createWithTotalParticles(50);
+    addChild(_emitter, 10);
+    _emitter->setTexture(Director::getInstance()->getTextureCache()->addImage("CloseNormal.png"));
+    _emitter->setAnchorPoint(Point(0, 0));
+    // duration
+    _emitter->setDuration(ParticleSystem::DURATION_INFINITY);
+    
+    // radius mode
+    _emitter->setEmitterMode(ParticleSystem::Mode::RADIUS);
+    
+    // radius mode: start and end radius in pixels
+    _emitter->setStartRadius(4);
+    _emitter->setStartRadiusVar(1);
+    _emitter->setEndRadius(ParticleSystem::START_RADIUS_EQUAL_TO_END_RADIUS);
+    _emitter->setEndRadiusVar(0);
+    
+    // radius mode: degrees per second
+    _emitter->setRotatePerSecond(100);
+    _emitter->setRotatePerSecondVar(0);
+    
+    // angle
+    _emitter->setAngle(90);
+    _emitter->setAngleVar(0);
+    
+    // emitter position
+    auto size = Director::getInstance()->getWinSize();
+    _emitter->setPosVar(Point::ZERO);
+    
+    // life of particles
+    _emitter->setLife(0.5);
+    _emitter->setLifeVar(0);
+    
+    // spin of particles
+    _emitter->setStartSpin(0);
+    _emitter->setStartSpinVar(0);
+    _emitter->setEndSpin(0);
+    _emitter->setEndSpinVar(0);
+    
+    // color of particles
+    Color4F startColor(0.0f, 0.8f, 0.9f, 1.0f);
+    _emitter->setStartColor(startColor);
+    
+    Color4F startColorVar(0, 0, 0, 1.0f);
+    _emitter->setStartColorVar(startColorVar);
+    
+    Color4F endColor(1.0f, 1.0f, 1.0f, 0.1f);
+    _emitter->setEndColor(endColor);
+    
+    Color4F endColorVar(0, 0, 0, 0.1f);
+    _emitter->setEndColorVar(endColorVar);
+    
+    // size, in pixels
+    _emitter->setStartSize(20);
+    _emitter->setStartSizeVar(1);
+    _emitter->setEndSize(0);
+    
+    // emits per second
+    _emitter->setEmissionRate(_emitter->getTotalParticles() / _emitter->getLife());
+    
+    // additive
+    _emitter->setBlendAdditive(false);
+    
+    //addChild(_emitter);
+    _emitter->setPosition(Point(200,200));
+    
+    
+    
+    
+    
     // 触摸移动时触发
     listener1->onTouchMoved = [](Touch* touch, Event* event){
         log("+++++++++");
